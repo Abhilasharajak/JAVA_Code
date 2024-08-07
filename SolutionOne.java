47cff0662cc93605fa9a9bf6966888c1950d813b
Abhilasharajak <132184018+Abhilasharajak@users.noreply.github.com>
2024-08-08 00:06:08 +0530

    Add files via upload

---
+++ SolutionOne.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class SolutionOne {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1}; // Return -1, -1 if no solution is found
+    }
+
+    public static void main(String[] args) {
+        SolutionOne ts = new SolutionOne();
+        int[] nums = {7, 11, 15,2};
+        int target = 9;
+        int[] result = ts.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
---
+++ SolutionSevenOne.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SolutionSevenOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+        String binary = convertToBinary(decimal);
+        System.out.println("Binary representation: " + binary);
+        sc.close();
+    }
+
+    public static String convertToBinary(int decimal) {
+        StringBuilder binary = new StringBuilder();
+        while (decimal > 0) {
+            binary.insert(0, decimal % 2);
+            decimal = decimal / 2;
+        }
+        return binary.toString();
+
+    }
+
+}
