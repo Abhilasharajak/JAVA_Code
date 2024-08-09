b81f57eca2fa4ffad27ff90226b76dba591e49c3
Abhilasha <abhilasharajak15@gmail.com>
2024-08-10 00:17:02 +0530

    First and last Occurance

--- SolutionEight.java
+++ SolutionEight.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class SolutionEight {
     public static void findfirstAndLast(int a[], int x){
          int first=-1, last=-1;
@@ -18,11 +16,7 @@ public class SolutionEight {
          }
     }
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
         int[] a= {1,2,3,5,5,6,7,5};
-        for(int i=0; i<a.length; i++){
-            a[i]=sc.nextInt();
-        }
         int x=5;
         findfirstAndLast(a,x);
     }
