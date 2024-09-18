f9075e5a83b718c4ec00c1db758f01e802b079fd
Abhilasha <abhilasharajak15@gmail.com>
2024-09-18 23:44:52 +0530

    add program

--- SolutionSevenTwo.java
+++ SolutionSevenTwo.java
@@ -1,25 +1,25 @@
 
 import java.util.*;
-public class SolutionSevenTwo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a decimal number: ");
-        int decimal = sc.nextInt();
-        String octal = convertToOctal(decimal);
-        System.out.println("Octal representation: " + octal);
-        sc.close();
-    }
+// public class SolutionSevenTwo {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a decimal number: ");
+//         int decimal = sc.nextInt();
+//         String octal = convertToOctal(decimal);
+//         System.out.println("Octal representation: " + octal);
+//         sc.close();
+//     }
 
-    public static String convertToOctal(int decimal) {
-        StringBuilder octal = new StringBuilder();
-        while (decimal > 0) {
-            octal.insert(0, decimal % 8);
-            decimal = decimal / 8;
-        }
-        return octal.toString();
+//     public static String convertToOctal(int decimal) {
+//         StringBuilder octal = new StringBuilder();
+//         while (decimal > 0) {
+//             octal.insert(0, decimal % 8);
+//             decimal = decimal / 8;
+//         }
+//         return octal.toString();
 
-    }
-}
+//     }
+// }
 import java.util.*;
 public class SolutionSevenTwo {
     public static void main(String[] args) {
